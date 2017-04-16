ff302904eaa4b3bf562cf5e0403833ee6f448c49
Zhuangziyao <921137471@qq.com>
2017-04-16 20:00:47 +0800

    add

--- project3/js/index.js
+++ project3/js/index.js
@@ -4,24 +4,39 @@
  * @date    2017-04-13 16:16:40
  * @version $Id$
  */
+$(document).ready(function(){
+
+//No.1
+$(function(){
+	var number=10;
+	var i;
+	for(i=1;i<=number;i++){
+		$('<img src=""/>').appendTo('.ui-project1-inner');
+	}
+	$(".ui-project1-inner").children().addClass('ui-project1-inner-pic');
+
+	for(i=0;i<number;i++)
+		$(".ui-project1-inner-pic").eq(i).attr("src","img/img-"+(i+1)+".jpg");
+})
 
 $(".ui-project1-inner-pic").click(function(event) {
 	$(".ui-project1-outer-img").attr("src",$(this).attr("src"));
 	$(".ui-project1-outer-img").css({'left':($(window).width()-400)/2,'top':($(window).height()-$(this).height()*(600/$(this).width()))/2});
-	$(".ui-project1-outer").fadeIn('fast');	/* Act on the event */
-});
-
-$(".ui-project1-outer").click(function(event) {
+	$(".ui-project1-outer").fadeIn('2000');
+	/* Act on the event */
+	$(".ui-project1-outer").click(function(event) {
 	$(this).fadeOut("fast");	/* Act on the event */
+	});
 });
 
-
+//No.2
 $(".ui-project2-title").click(function(event) {
 	$(this).addClass('title-current').siblings('.ui-project2-title').removeClass('title-current');
 	$(".ui-project2-content:eq("+$(this).index()+")").show().siblings('.ui-project2-content').hide();
 		/* Act on the event */
 });
 
+//No.3
 $(".ui-project3-row-del").click(function(event) {
 	var a=parseInt($(this).parent().children('.ui-project3-row-no').text())-1;
 	$(this).parent().nextAll().children('.ui-project3-row-no').text(function(){
@@ -52,4 +67,4 @@ $(".ui-project3-add").click(function(event) {
 	});
 		/* Act on the event */
 });
-
+});
